469c24dd3656aa407c1ffcab60ce7b7a2f4742d6
itsbharti <140591527+itsbharti@users.noreply.github.com>
2024-07-21 12:19:42 +0530

    Add/update CSS styling

--- src/components/Sidebar/SidebarIndex.js
+++ src/components/Sidebar/SidebarIndex.js
@@ -1,31 +1,70 @@
+import React, {useState} from "react";
 import Button from '@mui/material/Button';
 import { RxDashboard } from "react-icons/rx";
+import { IoChatbubbleEllipsesOutline } from "react-icons/io5";
+import { CiSettings } from "react-icons/ci";
+import { IoPeopleOutline } from "react-icons/io5";
+import { PiTelevision } from "react-icons/pi";
+import { CgFormatLeft } from "react-icons/cg";
+
 
 const Sidebar = () => {
+    const [activeItem, setActiveItem] = useState('/');
+
+    const handleItemClick = (path) => {
+        setActiveItem(path);
+    }
     return(
         <>
             <div className="sidebar">
                 <ul>
                     <li>
-                        <Button className='w-100 sidebarmenu'><span className='icon'><RxDashboard /></span> Dashboard</Button>
+                        <Button className= {`w-100 sidebarmenu ${activeItem === '/' ? 'active' : ''}`} 
+                            onClick={() => handleItemClick('/')}>
+                            <span className='icon'><RxDashboard className="icn" ></RxDashboard></span>
+                             Dashboard
+                        </Button>
                     </li>
 
                     <li>
-                        <Button className='w-100 sidebarmenu'><span className='icon'><RxDashboard /></span> Conversation</Button>
+                        <Button className={`w-100 sidebarmenu ${activeItem === '/conversation' ? 'active' : ''}`} 
+                            onClick={() => handleItemClick('/conversation')}>
+                            <span className='icon'> <IoChatbubbleEllipsesOutline className="icn"></IoChatbubbleEllipsesOutline>
+                            </span> 
+                            Conversation
+                        </Button>
                     </li>
 
                     <li>
-                        <Button className='w-100 sidebarmenu'><span className='icon'><RxDashboard /></span> Broadcasting</Button>
-                    </li>
-                    <li>
-                        <Button className='w-100 sidebarmenu'><span className='icon'><RxDashboard /></span> Template</Button>
-                    </li>
-                    <li>
-                        <Button className='w-100 sidebarmenu'><span className='icon'><RxDashboard /></span> Contact</Button>
+                        <Button className={`w-100 sidebarmenu ${activeItem === '/broadcasting' ? 'active' : ''}`} 
+                            onClick={() => handleItemClick('/broadcasting')}>
+                            <span className='icon'> <PiTelevision className="icn"></PiTelevision></span> 
+                            Broadcasting
+                        </Button>
                     </li>
 
                     <li>
-                        <Button className='w-100 sidebarmenu'><span className='icon'><RxDashboard /></span> Setting</Button>
+                        <Button className={`w-100 sidebarmenu ${activeItem === '/template' ? 'active' : ''}`} 
+                            onClick={() => handleItemClick('/template')}>
+                            <span className='icon'><CgFormatLeft className="icn" ></CgFormatLeft></span>
+                            Template
+                        </Button>
+                    </li>
+                    <li>
+                        <Button  className={`w-100 sidebarmenu ${activeItem === '/contact' ? 'active' : ''}`} 
+                            onClick={() => handleItemClick('/contact')}>
+                            <span className='icon'><IoPeopleOutline className="icn" ></IoPeopleOutline></span> 
+                            Contact
+                        </Button>
+                    </li>
+
+                    <li>
+                        <Button 
+                            className={`w-100 sidebarmenu ${activeItem === '/setting' ? 'active' : ''}`} 
+                            onClick={() => handleItemClick('/setting')}>
+                            <span className='icon'><CiSettings className="icn"></CiSettings> </span>
+                             Setting
+                        </Button>
                     </li>
                 </ul>
             </div>
